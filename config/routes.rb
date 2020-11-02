8c71b039c2575283269df1e6aa4e4118f370d4aa
Marta Simas <azevedo_m@outlook.com>
2020-11-02 23:34:26 +0000

    app working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# get 'questions/ask'
+get '/ask', to: 'questions#ask'
+# verb 'path', to: 'cntroller#action'
+get '/answer', to: 'questions#answer'
 end
